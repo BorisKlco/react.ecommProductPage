1996d52a8b30f8dc27263a273c4c941799a350ba
Boris Klco <klco@pm.me>
2023-06-30 08:21:03 +0000

    lost again...

--- src/Collections/Collections.jsx
+++ src/Collections/Collections.jsx
@@ -13,7 +13,24 @@ const Collections = () => {
 
   return (
     <div className="collections">
-      <h1>This is Collections</h1>
+      <div className="collection">
+        <img
+          src="https://img01.ztat.net/banner/8bdcd3b95d5248908e4e0a1122b6df31/bc8485fb42a74d03a4f667a373d2d152.jpg"
+          alt=""
+        />
+      </div>
+      <div className="collection">
+        <img
+          src="https://img01.ztat.net/banner/3b3ee2d45ec44ebca184ed7ab8a31ce3/d04af32b4bb647fea4e2af4db1e140bd.jpg"
+          alt=""
+        />
+      </div>
+      <div className="collectionKids">
+        <img
+          src="https://img01.ztat.net/banner/84090e6b29074e27b26658ee389bc935/dd3cf8f395aa4829a2e5a762281c6f02.jpg"
+          alt=""
+        />
+      </div>
     </div>
   );
 };
